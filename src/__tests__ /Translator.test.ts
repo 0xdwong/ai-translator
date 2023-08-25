1b400dd2b60527b22af0f9d476a2e0fdf7395a5f
0xdwong <0xdwong@gmail.com>
2023-08-25 14:20:57 +0800

    fix: 删除key

--- src/__tests__ /Translator.test.ts
+++ src/__tests__ /Translator.test.ts
@@ -1,7 +1,7 @@
 import * as chatgptTranslator  from '../index';
 
 test('My Greeter', async() => {
-    process.env.OPENAI_API_KEY = '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';
+    process.env.OPENAI_API_KEY = ''; // enter your key
 
     const translation = await chatgptTranslator.translate('hello world');
    console.log('====translation====', translation);
